baa7928a60aaf2339ae8c00e0d23977b0b3ecad4
Jyoti-EVDPL <121219411+Jyoti-EVDPL@users.noreply.github.com>
2023-09-20 19:27:02 +0530

    EOD

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,50 @@
 <script>
 	// Form Handling (Part 1)
+	const formValues = {
+		name:'',
+		profileSummary:'',
+		country:'',
+		jobLocation:[],
+	}
 
 </script>
 
 <main>
 	<!-- Form Handling (Part 1) -->
+	<div>
+		<pre>
+			{JSON.stringify(formValues,null,2)}
+		</pre>
+	</div>
+	<form action="">
+		<div>
+			<label for="Name">Name</label>
+			<input type="text" id="name" bind:value={formValues.name}/>
+		</div>
+		<div>
+			<label for="Profile">Profile Summary</label>
+			<textarea name="" id="profile" cols="30" rows="10" bind:value={formValues.profileSummary}/>
+		</div>
+		<div>
+			<label for="country">Country</label>
+			<select name="" id="country" bind:value={formValues.country}>
+				<option value="">Select a country</option>
+				<option value="india">india</option>
+				<option value="china">china</option>
+				<option value="brazil">brazil</option>
+			</select>
+		</div>
+		<div>
+			<label for="country">Job </label>
+			<select name="" id="country" bind:value={formValues.country}>
+				<option value="">Select a country</option>
+				<option value="india">india</option>
+				<option value="china">china</option>
+				<option value="brazil">brazil</option>
+			</select>
+		</div>
+
+	</form>
 
 
 </main>
@@ -14,7 +54,7 @@
 		display: inline-flex;
 	}
 	main {
-		text-align: center;
+		/* text-align: center; */
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
