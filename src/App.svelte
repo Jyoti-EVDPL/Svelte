907c68badd9c45ec04f4b8a7bdf04a0a169b309b
Jyoti-EVDPL <121219411+Jyoti-EVDPL@users.noreply.github.com>
2023-09-21 11:19:38 +0530

    Svelte Form Handling

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,19 @@
 		country:'',
 		jobLocation:[],
 	}
+	// Form Handling (Part 2)
+	const formValues2 = {
+		remoteWork:false,
+		skillSet:[],
+		yearsOfExp:'',
+	}
+	function submitForm(event){
+		event.preventDefault(); //Or we can do this ny event modifier by using pipe(below function)
+		console.log(formValues);
+	}
+	function submitForm2(){
+		console.log(formValues2);
+	}
 
 </script>
 
@@ -16,7 +29,7 @@
 			{JSON.stringify(formValues,null,2)}
 		</pre>
 	</div>
-	<form action="">
+	<form on:submit={submitForm}>
 		<div>
 			<label for="Name">Name</label>
 			<input type="text" id="name" bind:value={formValues.name}/>
@@ -35,13 +48,56 @@
 			</select>
 		</div>
 		<div>
-			<label for="country">Job </label>
-			<select name="" id="country" bind:value={formValues.country}>
+			<label for="job-location">Job Location</label>
+			<select name="" id="job-location" bind:value={formValues.jobLocation} multiple>
 				<option value="">Select a country</option>
 				<option value="india">india</option>
 				<option value="china">china</option>
 				<option value="brazil">brazil</option>
 			</select>
+		</div>
+		<div>
+			<button>Submit</button>
+		</div>
+	</form>
+<hr>
+	<!-- Form Handling (Part 2) -->
+	<div>
+		<pre>
+			{JSON.stringify(formValues2,null,2)}
+		</pre>
+	</div>
+	<form on:submit|preventDefault={submitForm2}>
+		<div>
+			<!-- <input type/> -->
+			<input type="checkbox" id="remoteWork" bind:checked={formValues2.remoteWork}>
+			<label for="remoteWork">Open To Remote Work?</label>
+
+		</div>
+		<div>
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label>Skill Set</label>
+			<input type="checkbox" id="html" value="html" bind:group={formValues2.skillSet}>
+			<label for="skillSet">Html</label>
+			<input type="checkbox" id="css" value="css" bind:group={formValues2.skillSet}>
+			<label for="skillSet">Css</label>
+			<input type="checkbox" id="js" value="js" bind:group={formValues2.skillSet}>
+			<label for="skillSet">JS</label>
+		</div>
+
+		<div>
+			<label>Years OF Experiance</label>
+			<input type="radio" id="0-2" value="0-2" bind:group={formValues2.yearsOfExp}>
+			<label for="0-2">0-2</label>
+			<input type="radio" id="3-5" value="3-5" bind:group={formValues2.yearsOfExp}>
+			<label for="3-5">3-5</label>
+			<input type="radio" id="6-10" value="6-10" bind:group={formValues2.yearsOfExp}>
+			<label for="6-10">6-10</label>
+			<input type="radio" id="10+" value="10+" bind:group={formValues2.yearsOfExp}>
+			<label for="10+">10+</label>
+		</div>
+		<div>
+			<button>Submit2</button>
 		</div>
 
 	</form>
